ef05022b4b5d1574763ede03f76dbac8b04eee2b
ariktisar <ariktisar@gmail.com>
2021-12-17 02:40:40 +0600

    12 commit

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -27,7 +27,18 @@ export default class RegisterScreen extends Component {
 
     }
 
-  
+    handlePickAvatar=async()=>{
+       // UserPermissions.getPhotoPermissions()
+        let result=await ImagePicker.launchImageLibraryAsync({
+            mediaTypes:ImagePicker.MediaTypeOptions.Images,
+            allowsEditing:true,
+            aspect:[4,3]
+        })
+        if(!result.cancelled){
+            console.log(result.uri)
+            this.setState({ user: { ...this.state.user, avatar: result.uri } });
+        }
+    }
 
                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
@@ -48,7 +59,17 @@ export default class RegisterScreen extends Component {
             <Ionicons name='ios-arrow-round-back' size={32} color="#FFF"></Ionicons>
         </TouchableOpacity>
         <View>
-       
+        <View style={{position:'absolute',alignItems:'center',width:'100%'}}>
+        <Text style={styles.greeting}> {`Hello! \n Sign up to get started.`} </Text>
+            <TouchableOpacity style={styles.avatarPlaceholder}onPress={this.handlePickAvatar}>
+                <Image source={{uri:this.state.user.avatar}} style={styles.avatar}/>
+                <Ionicons name="ios-add" size={32} color="#FFF" style={{marginTop:6,marginLeft:2}}>
+
+                </Ionicons>
+            </TouchableOpacity>
+            {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
+
+        </View>
 
        
      
